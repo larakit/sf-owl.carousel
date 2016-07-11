e05ab838c4d304cc1ae8a079377fa3805815761f
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 19:36:46 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-owl.carousel')
+    ->setSourceDir('public')
+    ->cssPackage('assets/owl.carousel.min.css')
+    ->jsPackage('owl.carousel.min.js');
